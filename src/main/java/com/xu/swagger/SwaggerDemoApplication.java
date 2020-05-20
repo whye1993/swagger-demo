13d62e195281f27f54330cdfb309c9f20a6c7064
whye1993 <40166993+whye1993@users.noreply.github.com>
2020-05-20 22:55:27 +0800

    Update SwaggerDemoApplication.java

--- src/main/java/com/xu/swagger/SwaggerDemoApplication.java
+++ src/main/java/com/xu/swagger/SwaggerDemoApplication.java
@@ -2,7 +2,7 @@ package com.xu.swagger;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//启动类
 @SpringBootApplication
 public class SwaggerDemoApplication {
 
